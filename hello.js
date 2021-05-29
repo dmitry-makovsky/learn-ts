f92eaf7b0c9e9c84c60b71708b7c00ab48ac92cd
Dmitry <dima@makovsky.me>
2021-05-29 14:32:36 +0300

    Inital test commit

---
+++ hello.js
@@ -0,0 +1,8 @@
+// function greet(person: string, date: Date) {
+//   console.log(`Hello ${person}, today is ${date.toDateString()}!`);
+// }
+// greet("Maddison", new Date());
+var data = [1, 2, 3, 'some'];
+data.forEach(function (num) {
+    console.log(num);
+});
---
+++ hello.ts
@@ -0,0 +1,11 @@
+// function greet(person: string, date: Date) {
+//   console.log(`Hello ${person}, today is ${date.toDateString()}!`);
+// }
+
+// greet("Maddison", new Date());
+
+const data: number[] = [1, 2, 3, 'some'];
+
+data.forEach((num: number) => {
+  console.log(num);
+});
